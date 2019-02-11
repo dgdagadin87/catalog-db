3675fbca1bcc7236fde7884c83b23f908a846618
dgdagadin87 <dgdagadin87@mail.ru>
2019-02-11 17:08:31 +0300

    Scheme file was created

---
+++ schema.sql
@@ -0,0 +1,159 @@
+-- SCHEMA: catalog
+-- DROP SCHEMA catalog ;
+CREATE SCHEMA catalog
+    AUTHORIZATION postgres;
+
+-- DROP TABLE catalog.client;
+CREATE TABLE catalog.client
+(
+    client_id BIGSERIAL PRIMARY KEY NOT NULL,
+    client_name character varying(100) NOT NULL,
+    client_agent character varying(100),
+    client_email character varying(100),
+    client_phone character varying(100)
+)
+WITH (
+    OIDS = TRUE
+)
+TABLESPACE pg_default;
+
+-- DROP TABLE catalog.manager;
+CREATE TABLE catalog.manager
+(
+    manager_id SERIAL PRIMARY KEY NOT NULL,
+    manager_name character varying(100) NOT NULL
+)
+WITH (
+    OIDS = TRUE
+)
+TABLESPACE pg_default;
+
+-- DROP TABLE catalog.order_state;
+CREATE TABLE catalog.order_state
+(
+    order_state_id SERIAL PRIMARY KEY NOT NULL,
+    order_state_code character varying(50) NOT NULL,
+    order_state_name character varying(50) NOT NULL
+)
+WITH (
+    OIDS = TRUE
+)
+TABLESPACE pg_default;
+
+INSERT INTO catalog.order_state (order_state_code, order_state_name) VALUES
+    ('BOUNCED_DONE', 'Оплачен, выполнен'),
+    ('CANCELLED', 'Отменен'),
+    ('DONE', 'Выполнен'),
+    ('DONE_WITHOUT_BOUNCE', 'Выполнен без оплаты');
+
+-- DROP TABLE catalog.order_source;
+CREATE TABLE catalog.order_source
+(
+    order_source_id SERIAL PRIMARY KEY NOT NULL,
+    order_source_code character varying(50) NOT NULL,
+    order_source_name character varying(50) NOT NULL
+)
+WITH (
+    OIDS = TRUE
+)
+TABLESPACE pg_default;
+
+INSERT INTO catalog.order_source (order_source_code, order_source_name) VALUES
+    ('OUTSIDE_ARCHIVE', 'Чужой архив'),
+    ('INSIDE_ARCHIVE', 'Свой архив'),
+    ('OUTSIDE_PHOTOGRAPHY', 'Чужая съемка'),
+    ('INSIDE_PHOTOGRAPHY', 'Своя съемка'),
+    ('COMPOUND', 'Смесь');
+
+-- DROP TABLE catalog.order;
+CREATE TABLE catalog.order
+(
+    order_id BIGSERIAL PRIMARY KEY NOT NULL,
+    order_number character varying(100) NOT NULL,
+    order_create_date date NOT NULL,
+    order_complete_date date,
+    client_id BIGINT NOT NULL,
+    manager_id INTEGER NOT NULL,
+    order_state_id INTEGER NOT NULL,
+    order_source_id INTEGER NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES catalog.client (client_id),
+    FOREIGN KEY (manager_id) REFERENCES catalog.manager (manager_id),
+    FOREIGN KEY (order_state_id) REFERENCES catalog.order_state (order_state_id),
+    FOREIGN KEY (order_source_id) REFERENCES catalog.order_source (order_source_id)
+)
+WITH (
+    OIDS = TRUE
+)
+TABLESPACE pg_default;
+
+-- DROP TABLE catalog.scene;
+CREATE TABLE catalog.scene
+(
+    scene_id character varying(4000) PRIMARY KEY COLLATE pg_catalog."default" NOT NULL,
+    stereo_scene_id character varying(4000) COLLATE pg_catalog."default",
+    scene_platform_name character varying(50) NOT NULL,
+    scene_table_name character varying(50) NOT NULL,
+    scene_identity_value BIGINT NOT NULL
+)
+WITH (
+    OIDS = TRUE
+)
+TABLESPACE pg_default;
+
+-- DROP TABLE catalog.order_2_scene;
+CREATE TABLE catalog.order_2_scene
+(
+    order_2_scene_id BIGSERIAL PRIMARY KEY NOT NULL,
+    order_id BIGINT NOT NULL,
+    scene_id character varying(4000) COLLATE pg_catalog."default" NOT NULL,
+    UNIQUE (order_id, scene_id),
+    FOREIGN KEY (order_id) REFERENCES catalog.order (order_id),
+    FOREIGN KEY (scene_id) REFERENCES catalog.scene (scene_id)
+)
+WITH (
+    OIDS = TRUE
+)
+TABLESPACE pg_default;
+
+-- DROP TABLE catalog.parameter;
+CREATE TABLE catalog.parameter
+(
+    parameter_id BIGSERIAL PRIMARY KEY NOT NULL,
+    parameter_name character varying(50) NOT NULL,
+    parameter_table_name character varying(50) NOT NULL,
+    parameter_column_name character varying(50) NOT NULL
+)
+WITH (
+    OIDS = TRUE
+)
+TABLESPACE pg_default;
+
+-- DROP TABLE catalog.parameter_2_scene;
+CREATE TABLE catalog.parameter_2_scene
+(
+    parameter_2_scene_id BIGSERIAL PRIMARY KEY NOT NULL,
+    parameter_id BIGINT NOT NULL,
+    scene_id character varying(4000) COLLATE pg_catalog."default" NOT NULL,
+    UNIQUE (parameter_id, scene_id),
+    FOREIGN KEY (parameter_id) REFERENCES catalog.parameter (parameter_id),
+    FOREIGN KEY (scene_id) REFERENCES catalog.scene (scene_id)
+)
+WITH (
+    OIDS = TRUE
+)
+TABLESPACE pg_default;
+
+-- DROP TABLE catalog.scene_archive;
+CREATE TABLE catalog.scene_archive
+(
+    scene_archive_id BIGSERIAL PRIMARY KEY NOT NULL,
+    scene_archive_scene_id character varying(4000) COLLATE pg_catalog."default" NOT NULL,
+    scene_file_path  character varying(500) NOT NULL,
+    scene_file_order INTEGER DEFAULT 1,
+    UNIQUE (scene_archive_scene_id, scene_file_order),
+    FOREIGN KEY (scene_archive_scene_id) REFERENCES catalog.scene (scene_id)
+)
+WITH (
+    OIDS = TRUE
+)
+TABLESPACE pg_default;
